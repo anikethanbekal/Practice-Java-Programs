f357f4891375ad37a92595f1a5429a6d92aa41f7
anikethanbekal <54067661+anikethanbekal@users.noreply.github.com>
2021-11-01 21:57:36 +0530

    Delete com.practice directory
    Deleting duplicate directory as its renamed to com.java

--- com.practice/src/strings/RemoveSpecialCharacters.java
+++
@@ -1,9 +0,0 @@
-package strings;
-
-public class RemoveSpecialCharacters {
-	public static void main(String[] args) {
-		String str = "T!@#hi()(s is t*(*(^he st*(^$#@$%ring";
-		String pstr = str.replaceAll("[^a-z A-Z 0-9]", "");
-		System.out.println(pstr);
-	}
-}
