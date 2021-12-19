4e687130f94811b95764a630085b3af51c588cb8
Anikethan Bekal <54067661+anikethanbekal@users.noreply.github.com>
2021-12-19 08:43:59 +0530

    Added new approach

--- com.java/src/strings/DuplicateCharacters.java
+++ com.java/src/strings/DuplicateCharacters.java
@@ -1,8 +1,7 @@
 package strings;
 
 public class DuplicateCharacters {
-	public static void main(String[] args) {
-		String str = "This is duplicate characters check program";
+	public static void bruteforce(String str){
 		int count;
 		if (str == null) {
 			System.out.println("The given string is null");
@@ -25,5 +24,29 @@ public class DuplicateCharacters {
 				
 			}
 		}
+	}
+	public static void hashMap(String[] args) {
+		String str = "anikethan";
+		char[] ch = str.toCharArray();
+		//If  order is important use LinkedHashMap
+		//If order not imp use HashMap
+		//If natural order requried use TreeMap
+		Map<Character,Integer> map = new LinkedHashMap<Character,Integer>(); 
+	        for(int i = 0; i<ch.length;i++){
+	        if(!map.containsKey(ch[i])){
+	            map.put(ch[i], 1);
+	        }else{
+	            map.put(ch[i],map.get(ch[i])+1);
+	        }
+	    }
+	     for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+	         if(entry.getValue()>1){
+               		System.out.println(entry.getKey() + " = " + entry.getValue());
+		 }
+	     }
+	}
+	public static void main(String[] args) {
+		String str = "This is duplicate characters check program";
+		
 	}
 }
